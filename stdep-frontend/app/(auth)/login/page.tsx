6b9fd1a55c6c28e312cfa640979affb578925b62
Aidan <aidanhuang10@gmail.com>
2025-11-20 14:02:38 -0700

    Changed fetch to hardcoded link (replaced later with env file)

--- stdep-frontend/app/(auth)/login/page.tsx
+++ stdep-frontend/app/(auth)/login/page.tsx
@@ -18,7 +18,8 @@ export default function LoginPage() {
 
 		try {
 			const res = await fetch(
-				`${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/auth/login`,
+                // Replace with env variable later
+				"http://localhost:8000/api/v1/auth/login",
 				{
 					method: "POST",
 					headers: { "Content-Type": "application/json" },
